cff78f58133fb8efd0f320683834228666158690
Vedant Kulkarni <vedantkulkarni05910@gmail.com>
2026-02-26 00:08:26 +0530

    New(Async API Project):Fetching through api's async project.

---
+++ 9_Async_Practice/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+// NEW COUNTRIES API URL (use instead of the URL shown in videos):
+// https://restcountries.com/v2/name/portugal
+
+// NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
+// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+
+///////////////////////////////////////
+// const countryName = ''+prompt("Enter the country name ? ")
+const getCountryData = function(countryName) {
+    const request = new XMLHttpRequest();
+    request.open('GET',`https://restcountries.com/v3.1/name/${countryName}`);
+    request.send();
+    // console.log(request.responseText)
+    request.addEventListener('load',function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data)
+    
+    const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flags.svg}" />
+            <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.continents}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+            <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+            </div>
+        </article>
+            `
+
+        countriesContainer.insertAdjacentHTML('beforeend',html);
+        countriesContainer.style.opacity = 1;
+    })
+}
+
+
+// getCountryData(countryName);
+getCountryData('portugal')
+getCountryData('spain')
+
