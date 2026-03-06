f96aff4503ed5d255f8cf51031e4074a46b9d3d2
Vedant Kulkarni <vedantkulkarni05910@gmail.com>
2026-03-06 17:36:13 +0530

    React useState

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
 function App() {
-  const [activeCount,setActiveCount] = useState(0);
+  // localStorage.setItem('activeCount',0);
+  const [activeCount,setActiveCount] = useState(Number(localStorage.getItem('activeCount')));
   return (
     <>
       <div className="flex h-screen items-center justify-center">
-        <button className="m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>setActiveCount(activeCount+1)}>Click Me to Add : {activeCount}</button>
+        <button className="m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{
+          const newCount =  activeCount + 1;
+          setActiveCount(newCount);
+          localStorage.setItem('activeCount',newCount)
+        }}>Click Me to Add : {activeCount}</button>
         
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>setActiveCount(activeCount-1)}>Click Me to Reduce : {activeCount}</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{
+        const newCount =  activeCount - 1;
+        setActiveCount(newCount);
+        localStorage.setItem('activeCount',newCount)
+        }}>Click Me to Reduce : {activeCount}</button>
       </div>
     </>
   )
