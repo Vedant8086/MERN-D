775093871e6ad8f42e42cb82d54e7dab6fa8cb1d
Vedant Kulkarni <vedantkulkarni05910@gmail.com>
2026-03-06 23:10:33 +0530

    Reseted Counting Logic

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,25 +1,46 @@
 import { useState } from "react";
 
 function App() {
-  // localStorage.setItem('activeCount',0);
-  const [activeCount,setActiveCount] = useState(Number(localStorage.getItem('activeCount')));
+  const [activeCount, setActiveCount] = useState(Number(localStorage.getItem('activeCount')));
+  
   return (
-    <>
-      <div className="flex h-screen items-center justify-center">
-        <button className="m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{
-          const newCount =  activeCount + 1;
+    <div className="bg-gray-500 flex flex-col h-screen items-center justify-center gap-6">
+      
+      <button className="bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Counter : {activeCount}
+      </button>
+      
+      <div className="flex">
+        <button className="m-5 bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+          onClick={()=>{
+          const newCount = activeCount + 1;
           setActiveCount(newCount);
           localStorage.setItem('activeCount',newCount)
-        }}>Click Me to Add : {activeCount}</button>
+        }}>
+          +
+        </button>
         
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{
-        const newCount =  activeCount - 1;
-        setActiveCount(newCount);
-        localStorage.setItem('activeCount',newCount)
-        }}>Click Me to Reduce : {activeCount}</button>
+        <button className="m-5 bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+          onClick={()=>{
+          const newCount = activeCount - 1 > 0 ? activeCount - 1 : 0;
+          setActiveCount(newCount);
+          localStorage.setItem('activeCount',newCount)
+        }}>
+          -
+        </button>
+
+        <button className="m-5 bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+          onClick={()=>{
+          const newCount = 0;
+          setActiveCount(newCount);
+          localStorage.setItem('activeCount',newCount)
+        }}>
+          Reset
+        </button>
       </div>
-    </>
+
+    </div>
   )
 }
 
-export default App
+export default App;
